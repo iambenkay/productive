d2089d31842800df9dd482184575e2731d64b531
Benjamin Kayode <benjamin.node@gmail.com>
2019-06-25 16:11:25 +0100

    Updates

---
+++ precache-manifest.a4bdb3cb666d752233c143876f337f3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2149a9d06f2387a49aaca9c8159254ac",
+    "url": "/productive/index.html"
+  },
+  {
+    "revision": "5ff62420671d3d362d2b",
+    "url": "/productive/static/css/main.f4213a38.chunk.css"
+  },
+  {
+    "revision": "052ab6c69d495a116b44",
+    "url": "/productive/static/js/2.4237b512.chunk.js"
+  },
+  {
+    "revision": "5ff62420671d3d362d2b",
+    "url": "/productive/static/js/main.b2b0aad6.chunk.js"
+  },
+  {
+    "revision": "0fdea4d12cc214355cb4",
+    "url": "/productive/static/js/runtime~main.a0921305.js"
+  }
+]);
